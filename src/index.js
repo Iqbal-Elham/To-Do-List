ffaa6548e9c4195c9ef8dfb58f65bd1de732ff3b
iqbal-elham <iqbal.elham7@gmail.com>
2022-12-07 19:57:45 +0430

    Add function for rendering the tasks from array object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './style.css';
+
+const tasks = [
+  {
+    description: 'Reading 10 pages of book',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Learning JavaScript',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Finish my todo project',
+    completed: false,
+    index: 2,
+  },
+];
+
+let tasksList = '';
+
+const tasksUl = document.getElementById('tasksUl');
+
+const listRender = () => {
+  tasks.forEach((n) => {
+    tasksList += `
+  <li><p><span>${n.completed}</span> | ${n.description} | <span>${n.index}</span></p></li><hr>`;
+  });
+  tasksUl.innerHTML = tasksList;
+};
+
+window.addEventListener('load', listRender);
