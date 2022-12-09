6787d79f9f4858d0b8aea784ec450c5b9eaecc79
iqbal-elham <iqbal.elham7@gmail.com>
2022-12-10 03:15:51 +0430

    Clean the unused comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import './style.css';
 import listRender from './modules/displayUI.js';
-// import collection from './modules/collection.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem('todo')) {
     listRender(JSON.parse(localStorage.todo));
   }
 });
-
-// window.document.onload = listRender(collection.allTodo());
